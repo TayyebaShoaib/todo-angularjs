474b7842bcaeadc57b0c3a5875f8fdf33083c416
Tayyeba Shoaib <tayyeba.t@gmail.com>
2018-04-09 23:25:17 +0100

    use service to get data

--- scripts/app.js
+++ scripts/app.js
@@ -5,9 +5,19 @@ angular.module('todoListApp', [])
     $scope.learningNgChange = function() {
         console.log('An input changed!');
     };
+
+    dataService.getTodos(function(response) {
+        console.log(response.data);
+        $scope.todos = response.data;
+    });
 })
-.service('dataService', function() {
+.service('dataService', function($http) {
     this.helloConsole = function() {
         console.log('This is the hello console service!');
-    }
-})
+    };
+    this.getTodos = function(callback)
+                    {
+                        $http.get('mock/todos.json')
+                        .then(callback);
+                    }
+});
