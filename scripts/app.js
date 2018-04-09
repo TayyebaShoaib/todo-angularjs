a1b6852888abd4fde0e35c72f835d632b3fff54f
TayyebaShoaib <Tayyeba.s@pearl-dme.com>
2018-04-09 17:59:34 +0100

    user service to get data

--- scripts/app.js
+++ scripts/app.js
@@ -5,15 +5,6 @@ angular.module('todoListApp', [])
     $scope.learningNgChange = function() {
         console.log('An input changed!');
     };
-
-    $scope.todos = [
-        {"name": "clean the house"},
-        {"name": "water the dog"},
-        {"name": "feed the lawn"},
-        {"name": "pay dem bills"},
-        {"name": "run"},
-        {"name": "swim"}
-    ];
 })
 .service('dataService', function() {
     this.helloConsole = function() {
